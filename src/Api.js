3f33dfb006cedace23be1c717549df6f26b16589
Youri Francois <yourifrancois97@gmail.com>
2021-01-08 16:59:25 -0500

    work on the front end make more user friendly

---
+++ src/Api.js
@@ -0,0 +1,7 @@
+const api= {
+    base:"https://api.openweathermap.org/data/2.5/weather?q=",
+    key:"8251735ab44064559bf27836d96c9d42"
+  }
+
+
+  module.exports = {api}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 
 import './App.css';
+import {api}from './Api'
 import React,{useState} from 'react'
 
+let click=true;
 
 function App() {
 const [data, setData] = useState({})
 const [text, setText] = useState('');
-const api= {
-  base:"api.openweathermap.org/data/2.5/weather?zip=",
-  key:"8251735ab44064559bf27836d96c9d42"
+const [deg, setDeg] = useState('units=imperial');
+
+
+const change =()=> {
+  click=!click;
+  console.log("click "+click)
+  if(click){setDeg('units=metric')}
+  else{setDeg('units=imperial')}
+  console.log('deg= '+deg)
+  getapi()
 }
+const getapi =()=>{
+fetch(`${api.base}${text}&${deg}&appid=${api.key}`)
+.then(res => res.json())
+.then(result => {
+ setData(result);
+
+});}
 
 const getData = evt => {
   if (evt.key === "Enter"){
- fetch(`https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${api.key}`)
- .then(res => res.json())
- .then(result => {
-  setData(result);
-  setText('');
- });
+    getapi()
 }
 }
+
 
   return (
     <div className="App">
@@ -34,16 +46,19 @@ const getData = evt => {
             value={text}
             onKeyPress={getData}
           />
+           change <button onClick={change}>{deg==="units=metric"?"*F":"*C"}</button>
         </div>
 
+     
+
      {(typeof data.main != 'undefined')?
-<div>
-    <div>In       : {data.name}</div>
-     <div>Temperature      : {data.main.temp}</div>
-     <div>description      : {data.weather[0].description}</div>
-     <div>Temperature Min  : {data.main.temp_min}</div>
-     <div>Temperature Max  : {data.main.temp_max}</div>
-     <div>humidity         : {data.main.humidity}</div>
+<div className="detail">
+    <div className="detailBox">In       : {data.name}</div>
+     <div className="detailBox">Temperature      : { Math.round(data.main.temp)}{deg==="units=metric"?"*F":"*C"}</div>
+     <div className="detailBox">Description      : {data.weather[0].description}</div>
+     <div className="detailBox">Temperature Min  : {Math.round(data.main.temp_min)}{deg==="units=metric"?"*F":"*C"}</div>
+     <div className="detailBox">Temperature Max  : {Math.round(data.main.temp_max)}{deg==="units=metric"?"*F":"*C"}</div>
+     <div className="detailBox">humidity         : {data.main.humidity}</div>
 </div>
      :("enter city name")}
 
