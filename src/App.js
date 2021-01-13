1454c500e7dc01171b239b86d0c3c2da39a5b429
Youri Francois <yourifrancois97@gmail.com>
2021-01-13 13:49:22 -0500

    Fix some bug in the display and  work in the css

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ const getData = evt => {
 
   return (
     <div className="App">
+      <h1>Today Weather </h1>
 
  <div className="search-box">
           <input 
@@ -46,7 +47,7 @@ const getData = evt => {
             value={text}
             onKeyPress={getData}
           />
-           change <button onClick={change}>{deg==="units=metric"?"*F":"*C"}</button>
+           change to ->  <button onClick={change}>{click?"*C":"*F"}</button>
         </div>
 
      
@@ -54,10 +55,10 @@ const getData = evt => {
      {(typeof data.main != 'undefined')?
 <div className="detail">
     <div className="detailBox">In       : {data.name}</div>
-     <div className="detailBox">Temperature      : { Math.round(data.main.temp)}{deg==="units=metric"?"*F":"*C"}</div>
+     <div className="detailBox">Temperature      : { Math.round(data.main.temp)}{click?"*F":"*C"}</div>
      <div className="detailBox">Description      : {data.weather[0].description}</div>
-     <div className="detailBox">Temperature Min  : {Math.round(data.main.temp_min)}{deg==="units=metric"?"*F":"*C"}</div>
-     <div className="detailBox">Temperature Max  : {Math.round(data.main.temp_max)}{deg==="units=metric"?"*F":"*C"}</div>
+     <div className="detailBox">Temperature Min  : {Math.round(data.main.temp_min)}{click?"*F":"*C"}</div>
+     <div className="detailBox">Temperature Max  : {Math.round(data.main.temp_max)}{click?"*F":"*C"}</div>
      <div className="detailBox">humidity         : {data.main.humidity}</div>
 </div>
      :("enter city name")}
